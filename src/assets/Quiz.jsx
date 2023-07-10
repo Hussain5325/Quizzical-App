ae4b118a6642e88faaea762a2bcb798a39a93821
Hussain Murtaza <109164058+Hussain760@users.noreply.github.com>
2023-07-10 10:38:24 +0500

    Update Quiz.jsx

--- src/assets/Quiz.jsx
+++ src/assets/Quiz.jsx
@@ -15,7 +15,7 @@ export default function Quiz({ data, score, render }) {
       e.target.style.backgroundImage = 'url(/cross.svg)'
     }
 
-    const box = document.querySelector('.question-box')
+    const box = document.querySelectorAll('.option')
     box.style.pointerEvents = 'none'
     box.style.backgroundColor = 'blue'
   }
